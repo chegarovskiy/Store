cbb8ba7f2f4820e06497e69bbdbc187d0f85e4c9
chegarovskiy.vlad <chegarovskiy.vlad@gmail.com>
2017-01-07 16:19:09 +0200

    Add connection string to database in Web.config

--- Spare.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ Spare.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -37,15 +37,17 @@ namespace Spare.WebUI.Infrastructure
         private void AddBindings()
         {
             // здесь буду привязки (зависимости)
-            Mock<IProductRepository> mock = new Mock<IProductRepository>();
-            mock.Setup(m => m.Products).Returns(new List<Product>
-            {
-                new Product {Name = "шаровая опора", Price = 150},
-                new Product {Name = "рулевая тяга", Price = 200},
-                new Product {Name = "рулевой наконечник", Price = 250},
-                new Product {Name = "сайлентблок рычага", Price = 300}
-            });
-            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+            //Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            //mock.Setup(m => m.Products).Returns(new List<Product>
+            //{
+            //    new Product {Name = "шаровая опора", Price = 150},
+            //    new Product {Name = "рулевая тяга", Price = 200},
+            //    new Product {Name = "рулевой наконечник", Price = 250},
+            //    new Product {Name = "сайлентблок рычага", Price = 300}
+            //});
+            //kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+
+            kernel.Bind<IProductRepository>().To<IProductRepository>();
         }
 
     }
