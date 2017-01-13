aa2d00980bcdaa7dce495332278304264265d02e
chegarovskiy.vlad <chegarovskiy.vlad@gmail.com>
2017-01-13 15:07:32 +0200

    add Class Cart, CartLine to Entitys and add some mesods for work with cart(Add, Remove, Comput, Clear). Create StyleSheet.css

---
+++ Store.Domain/Entitys/Cart.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Store.Domain.Entitys
+{
+    public class CartLine
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+
+    public class Cart
+    {
+
+         private List<CartLine> lineCollection = new List<CartLine>();
+
+        public IEnumerable<CartLine> Lines
+        {
+            get { return lineCollection; } 
+        } 
+
+        // adding new product to cart or exchange it quantity in cart 
+        public void AddItem(Product product, int quantity)
+        {
+            CartLine line = lineCollection.Where(p => p.Product.ProductId == product.ProductId).FirstOrDefault();
+
+            if (line == null)
+            {
+                lineCollection.Add(new CartLine
+                {
+                    Product = product,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        public void RemoveLine(Product product)
+        {
+            lineCollection.RemoveAll(l => l.Product.ProductId == product.ProductId);
+        }
+
+        public decimal ComputTotalValue()
+        {
+            return lineCollection.Sum(l => l.Product.Price*l.Quantity);
+        }
+
+        public void Clear()
+        {
+            lineCollection.Clear();
+        }
+    }
+
+}
